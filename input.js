ecbf305ca4cc33b283689c3d8034c6d77ce0bf47
N1KF <40588878+N1KF@users.noreply.github.com>
2020-11-24 23:06:26 -0600

    Default block is now random

--- input.js
+++ input.js
@@ -1,5 +1,4 @@
-var canvas = document.getElementById("game");
-var b = 10;
+var b = random(9,23);
 var bshift = 0;
 
 var mousex;
@@ -31,8 +30,8 @@ function getMousePos(mouseEvent)
 	pos.x = window.event.x + document.body.scrollLeft - 2;
 	pos.y = window.event.y + document.body.scrollTop  - 2;
 	}
-	pos.x -= obj_left;
-	pos.y -= obj_top;
+	pos.x -= obj_left + GUI[0].cx;
+	pos.y -= obj_top  + GUI[0].cy;
 	return pos;
 }
 
@@ -49,6 +48,10 @@ canvas.onmouseup = function(){
 	isEditHolding = false;
 }
 
+canvas.oncontextmenu = function(){
+	return false;
+}
+
 function updateMousePos(){
 	mousex = getMousePos(event).x; // event.x - canvas.offsetLeft;
 	mousey = getMousePos(event).y; // event.y - canvas.offsetTop;
@@ -56,21 +59,8 @@ function updateMousePos(){
 
 function editHold(){
 	if (isEditHolding === true){
-		var xtile = Math.floor((mousex + camera.x) / 16);
-		var ytile = Math.floor((mousey + camera.y) / 16);
-		/*canvas.onmousemove = function(){
-			mousex = event.x - canvas.offsetLeft;
-			mousey = event.y - canvas.offsetTop;
-			
-			if (xtile !== Math.floor(mousex / 16)){
-				xtile   = Math.floor(mousex / 16);
-			}
-			else if (ytile !== Math.floor(mousey / 16)){
-				ytile   = Math.floor(mousey / 16);
-			}
-			else return;
-		}
-		*/
+		var xtile = Math.floor((mousex + GUI[0].x) / x_vtile());
+		var ytile = Math.floor((mousey + GUI[0].y) / y_vtile());
 
 		if(xtile < 0 || xtile >= map.columns || ytile < 0 || ytile >= map.rows) return;
 		else if (rightClick === true) edit(1, xtile, ytile, bshift)
@@ -80,6 +70,15 @@ function editHold(){
 }
 
 onkeydown = function (){
+	if (event.key != ("F12")) event.preventDefault()
+	/*
+	var keyNumber = Number(event.key);
+	
+	if (keyNumber != NaN && typeof keyNumber === 'number'){
+		b = keyNumber;
+		return;
+	}*/
+	
 	switch(event.key){
 		case "ArrowRight":
 		case "d":
@@ -99,45 +98,37 @@ onkeydown = function (){
 			break;
 		case " ":
 			isJumping = true; // Placeholder
-			event.preventDefault();
 			break;
 		case "g":
-			if (!entities[0].isFlying) entities[0].isFlying = true;
-			else entities[0].isFlying = false;
+			if (!mainEntity.isFlying) mainEntity.isFlying = true;
+			else mainEntity.isFlying = false;
+			break;
+			
+		// Debug stuff
+			
+		case "p":
+			start(10);
+			break;
+		case "f":
+			canvas.requestFullscreen();
 			break;
 		case "[":
 			tick();
 			break;
 		case "]":
-			fastForward(2); break;
+			tick(2); break;
 		case "\\":
-			fastForward(25); break;
-		case "1":
-			b = 9; break;
-		case "2":
-			b = 10; break;
-		case "3":
-			b = 11; break;
-		case "4":
-			b = 12; break;
-		case "5":
-			b = 13; break;
-		case "6":
-			b = 14; break;
-		case "7":
-			b = 1; break;
-		case "8":
-			b = 2; break;
-		case "9":
-			b = 3; break;
-		case "0":
-			b = 4; break;
-		case "-":
-			if (b >  0) {b--;} else b = 97; break; // Placeholder
+			tick(25); break;
+		case "-": 
+			if (b >  0) b--;
+			//else b = 316;
+			break;
+		case "+":
 		case "=":
-			if (b < 97) {b++;} else b =  0; break; // Placeholder
-		case ";":
-			x.pos = 0; y.pos = 0; break;
+			if (b < 97) b++; break;
+			//316 for actual last block
+		case "r":
+			map.reset(); break;
 	}
 }
 
@@ -158,19 +149,15 @@ onkeyup   = function (){
 		case "ArrowUp":
 		case "w":
 			if(controlDown  <  1) controlDown  += 1;
+			break;
+		case " ":
+			isJumping = false; // Placeholder
+			break;
 	}
 }
 
 function reset(){
-	x.pos = 16;
-	x.v = 0;
-	x.cs = 0;
-	x.remainder = 0;
-	
-	y.pos = 16;
-	y.v = 0;
-	y.cs = 0;
-	y.remainder = 0;
+	entities[0].reset();
 	
 	time = 0;
 	draw();
